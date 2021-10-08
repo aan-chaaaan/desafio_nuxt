af98438403ea56c002e67b8edaac657c6b1ddce4
angela <angela.valdebenito.v@gmail.com>
2021-10-07 22:06:41 -0300

    first commit

---
+++ pages/Cards.vue
@@ -0,0 +1,29 @@
+<template>
+<v-app app>
+      <v-flex d-flex>
+      <v-layout wrap>
+        <v-flex
+          md4 
+        >
+        
+          <v-card  v-for="(personaje, index) in $store.state.personajes.data"
+          :key="index" class="container ma-4">
+            <v-img height="350" :src='personaje.imagen'></v-img>
+            <v-card-title>{{ personaje.nombre }}</v-card-title>
+          </v-card>
+        
+        </v-flex>
+      </v-layout>
+    </v-flex>
+  </v-app>
+</template>
+<script>
+  export default {
+    name: "Cards",
+    data: () => ({
+      
+    }),
+  }
+</script>
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+<v-app app>
+    <v-container>
+      <h2 class="text-center">DESAFIO NUXT</h2>
+    </v-container>
+</v-app>
+</template>
+<script>
+
+export default {
+  name: 'App',
+  components: {},
+  
+}
+</script>
