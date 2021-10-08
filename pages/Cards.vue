89f20505f352127ac1ab073273ff4b44926f82f7
angela <angela.valdebenito.v@gmail.com>
2021-10-07 22:24:19 -0300

    commit

--- pages/Cards.vue
+++ pages/Cards.vue
@@ -1,29 +1,25 @@
 <template>
-<v-app app>
-      <v-flex d-flex>
-      <v-layout wrap>
-        <v-flex
-          md4 
-        >
-        
-          <v-card  v-for="(personaje, index) in $store.state.personajes.data"
-          :key="index" class="container ma-4">
-            <v-img height="350" :src='personaje.imagen'></v-img>
+  <v-app app>
+    <v-row>
+      <v-col v-for="(personaje, index) in $store.state.personajes.data"
+            :key="index" cols="12" sm="3" md="4" >
+        <v-container fluid>
+          <v-card
+            
+            class="ma-4"
+          >
+            <v-img height="350" :src="personaje.imagen"></v-img>
             <v-card-title>{{ personaje.nombre }}</v-card-title>
           </v-card>
-        
-        </v-flex>
-      </v-layout>
-    </v-flex>
+        </v-container>
+      </v-col>
+    </v-row>
   </v-app>
 </template>
 <script>
-  export default {
-    name: "Cards",
-    data: () => ({
-      
-    }),
-  }
+export default {
+  name: 'Cards',
+  data: () => ({}),
+}
 </script>
-<style>
-</style>
+<style></style>
